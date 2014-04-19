73a6b5fec323dd6a7c32f568b430a45b4d415e75
Marcelo Politzer Couto <mpolitzer.c@gmail.com>
2014-04-19 04:40:45 -0300

    Updated API to be the same as gpio0

--- arch/msp430/common/dev/gpio1.h
+++ arch/msp430/common/dev/gpio1.h
@@ -10,12 +10,12 @@ static inline void gpio1_dtor(void)
 {
 }
 
-static inline void gpio1_setin(uint8_t pins)
+static inline void gpio1_set_in(uint8_t pins)
 {
 	P2DIR &= ~pins;
 }
 
-static inline void gpio1_setout(uint8_t pins)
+static inline void gpio1_set_out(uint8_t pins)
 {
 	P2DIR |= pins;
 }
@@ -25,17 +25,29 @@ static inline uint8_t gpio1_read(void)
 	return P2IN;
 }
 
-static inline void gpio1_sethigh(uint8_t pins)
+static inline void gpio1_set(uint8_t pins)
 {
 	P2OUT |= pins;
 }
 
-static inline void gpio1_setlow(uint8_t pins)
+static inline void gpio1_clr(uint8_t pins)
 {
 	P2OUT &=~pins;
 }
 
-static inline void gpio1_setto(uint8_t mask, uint8_t pins)
+static inline void gpio1_set_to(uint8_t pins, uint8_t values)
 {
-	bm_apply(P2OUT, mask, pins);
+	bm_apply(P2OUT, pins, values);
+}
+
+static inline void gpio1_set_pupd(uint8_t pins, uint8_t up)
+{
+	P2REN |= pins; /* pull (up/down) */
+	bm_apply(P2OUT, pins, up);
+}
+
+static inline void gpio1_set_edge_irq(uint8_t pins, uint8_t rise)
+{
+	P2IE |= pins;
+	bm_apply(P2IES, pins, rise);
 }
