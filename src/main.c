84158275925fdfbe88bd666fa8fd0ee92430a4cf
Marcelo Politzer Couto <mpolitzer.c@gmail.com>
2014-04-19 04:41:46 -0300

    updated main's gpio0 test.

--- src/main.c
+++ src/main.c
@@ -1,23 +1,23 @@
 #include <stdint.h>
 #include <dev/cpu0.h>
+#include <dev/gpio0.h>
+#include <dev/gpio1.h>
 
 void gpio0_irq(uint8_t v) {
+	gpio0_set_to(P1, ~(gpio0_read() & P1));
 }
 
-__attribute__((used))
+__attribute__((used)) /* LTO is trying to remove main... */
 int main(int argc, char *argv[]) {
 	cpu0_ctor();
-#if 0
 	gpio0_ctor();
-	gpio0_in(P0);
-	gpio0_out(P1);
 
-	gpio0_set(P1);
-	gpio0_clr(P1);
+	gpio0_set_in(P0);
 
-	gpio0_set(P1, P1);
+	gpio0_set_out(P1);
+	gpio0_set_edge_irq(P0,  0);
 
-	gpio0_irq_on_edges(P0, 0);
-#endif
+	while (1);
+
 	return 0;
 }
