e125560956d5fed023ffd57d87b6f80977e04521
Marcelo Politzer Couto <mpolitzer.c@gmail.com>
2014-05-10 03:19:32 -0300

    Implemented cpu essentials for milestone 0.1.
    There still are missing functions left as stubs to implement that
    are not required at the moment.

--- arch/arm/lm4f120/dev/cpu0.h
+++ arch/arm/lm4f120/dev/cpu0.h
@@ -28,4 +28,10 @@ static inline void cpu0_dtor(void)
 	cpu0_halt();
 }
 
+
+/* stubs, not required for milestone. */
+static inline void cpu0_reset(void) {}
+static inline void cpu0_busywait_ms(uint16_t ms) {}
+static inline uint8_t cpu0_boot_cause(void) { return CPU_BOOT_INVALID; }
+
 #endif /* CPU0_H */
