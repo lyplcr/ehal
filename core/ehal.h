4d9c433a0e014e13c2ce4593cf634fbace4c7af3
Marcelo Politzer Couto <mpolitzer.c@gmail.com>
2014-05-10 03:13:16 -0300

    Update of CPU interface.
    from issue: https://github.com/mpolitzer/ehal/issues/1

--- core/ehal.h
+++ core/ehal.h
@@ -17,18 +17,27 @@ enum {
 };
 
 /* devices ================================================================== */
-
 /* cpu ---------------------------------------------------------------------- */
-struct cpu_interface {
-	void    (*ctor)      (void);
-	void    (*dtor)      (void);
 
-	void    (*reset)     (void);
-	void    (*delay_ms)  (uint16_t ms);
-	uint8_t (*boot_cause)(void);
+#define CPU_BOOT_POWER_UP 0
+#define CPU_BOOT_SOFT     1
+#define CPU_BOOT_WDT      2
+#define CPU_BOOT_HARD     3
+
+struct cpu_interface {
+	void    (*ctor)        (void); /**< init pll, irqs, etc... */
+	void    (*dtor)        (void); /**< equivalent to halt. */
+	void    (*halt)        (void); /**< or stall, stop the execution. */
+
+	void    (*enable_irqs)(void);  /**< */
+	void    (*disable_irqs)(void); /**< */
+
+	void    (*reset)       (void);
+	void    (*busywait_ms) (uint16_t ms);
+	uint8_t (*boot_cause)  (void);
 };
 
-/* gpio---------------------------------------------------------------------- */
+/* gpio --------------------------------------------------------------------- */
 /* some type... depends on the number of pins per port. */
 typedef uint8_t gpio_mask;
 struct gpio_interface {
@@ -54,12 +63,33 @@ struct gpio_interface {
 	void (*set_opendrain)(gpio_mask pins);
 	void (*set_edge_irq) (gpio_mask rise, gpio_mask fall);
 };
+#if 0
+/* spi ---------------------------------------------------------------------- */
+
+#define SPI_R  (1<<0)
+#define SPI_W  (1<<1)
+#define SPI_RW (SPI_R | SPI_W)
+struct spi_pkt {
+	struct spi_pkt *next;
+	struct job *cont;
+	uint8_t rw;
+	uint8_t *buf;
+	uint16_t n;
+};
+
+struct spi_queue {
+	struct spi_pkt *head, **tail;
+};
 
 struct spi_interface {
 	void (*ctor)(uint8_t mode);
 	void (*dtor)(void);
 
 	uint8_t (*rw8)(uint8_t w);
+	void    (*rw8_assync)(struct spi_packet *pkt);
 };
+
+void spi_mkpkt(struct spi_pkt *, uint8_t rw, uint8_t *buf, uint16_t n);
+#endif
 
 #endif /* EHAL_H */
