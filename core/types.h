29daef18ba3412b09c82329386e379b15e568ba2
Marcelo Politzer Couto <mpolitzer.c@gmail.com>
2014-06-07 06:20:05 -0300

    bugfix: lack of parenthesis in the HWPTR mas causing trouble.

--- core/types.h
+++ core/types.h
@@ -3,7 +3,13 @@
 
 #include <stdint.h>
 #include <stdbool.h>
-#define HWPTR(x) ((volatile uint32_t *)x)
+#define HWPTR(x) ((volatile uint32_t * const)(x))
 #define HWREG(x) (*HWPTR(x))
+
+#define HWPTR16(x) ((volatile uint16_t * const)(x))
+#define HWREG16(x) (*HWPTR16(x))
+
+#define HWPTR8(x) ((volatile uint8_t * const)(x))
+#define HWREG8(x) (*HWPTR8(x))
 
 #endif /* TYPES_H */
